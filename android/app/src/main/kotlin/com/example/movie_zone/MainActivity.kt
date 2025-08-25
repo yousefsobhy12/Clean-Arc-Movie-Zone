c16ad8eed12e20d85a9ee25b9f60bce19dd74b9a
yousefsobhy12 <yousefsobhy124@gmail.com>
2025-08-26 02:32:48 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/movie_zone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.movie_zone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MovieZone());
+}
+
+class MovieZone extends StatelessWidget {
+  const MovieZone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold());
+  }
+}
