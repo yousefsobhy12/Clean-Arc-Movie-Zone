c31e471259ee831e6291182440cb57c78b756bf4
yousefsobhy12 <yousefsobhy124@gmail.com>
2025-09-10 03:43:40 +0300

    feat: create movie model and entity feat: add movie mapper in helper folder feat: implement data return in movie repository implementation

--- lib/data/movie/repositories/movie_repo_impl.dart
+++ lib/data/movie/repositories/movie_repo_impl.dart
@@ -1,10 +1,24 @@
 import 'package:dartz/dartz.dart';
+import 'package:movie_zone/common/helper/mapper/movie_mapper.dart';
+import 'package:movie_zone/data/movie/models/movie_model.dart';
+import 'package:movie_zone/data/movie/sources/movie_service.dart';
 import 'package:movie_zone/domain/movie/repositories/movie_repo.dart';
+import 'package:movie_zone/service_locator.dart';
 
 class MovieRepoImpl extends MovieRepo {
   @override
-  Future<Either> getTrendingMovies() {
-    // TODO: implement getTrendingMovies
-    throw UnimplementedError();
+  Future<Either> getTrendingMovies() async {
+    var returnedData = await serviceLocator<MovieService>().getTrendingMovies();
+    return returnedData.fold(
+      (error) {
+        return Left(error);
+      },
+      (data) {
+        var movies = List.from(data['content'])
+            .map((movie) => MovieMapper.toEntity(MovieModel.fromJson(movie)))
+            .toList();
+        return Right(movies);
+      },
+    );
   }
 }
